a50c9f82110913a4b48c288faba1820816de48b1
Tom Wilson <tom@hyper.io>
2022-09-27 21:38:21 +0000

    added profile stats

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,7 +1,14 @@
+import Account from 'arweave-account';
+import { path, prop } from 'ramda';
+import { atomicToStamp, winstonToAr, atomicToBar } from './utils.js'
+
+const GATEWAY = 'https://arweave.net'
 const REDSTONE_GATEWAY = 'https://gateway.redstone.finance'
 const TRADE_SOURCE_ID = 'BzNLxND_nJEMfcLWShyhU4i9BnzEWaATo6FYFsfsO0Q'
 const CACHE = 'https://cache.permapages.app'
 const STAMP_CONTRACT = 'aSMILD7cEJr93i7TAVzzMjtci_sGkXcWnqpDkG6UGcA'
+const BAR = 'mMffEC07TyoAFAI_O6q_nskj2bT8n4UFvckQ3yELeic'
+const account = new Account()
 
 export async function getAssetCount() {
   return fetch(`${REDSTONE_GATEWAY}/gateway/contracts-by-source?id=${TRADE_SOURCE_ID}`)
@@ -13,3 +20,91 @@ export async function getStampCount() {
   return fetch(`${CACHE}/${STAMP_CONTRACT}`).then(res => res.json())
     .then(data => Object.keys(data.stamps).length)
 }
+
+export async function getAccount(addr) {
+  return account.get(addr)
+  // if no account fallback to permapage
+
+}
+
+export async function getUserAssets(addr) {
+  const query = `
+  query {
+    transactions(
+      first: 100, 
+      owners: ["${addr}"],
+      tags: {name: "Contract-Src", values: ["${TRADE_SOURCE_ID}"]}) {
+        edges {
+          cursor
+          node {
+            id
+          }
+        }
+      }
+  }      
+        
+  `
+
+  return fetch(`${GATEWAY}/graphql`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query
+    })
+  }).then(res => res.ok ? res.json() : Promise.reject(new Error('Could not get data')))
+    .then(data => {
+      const edges = path(['data', 'transactions', 'edges'], data)
+      const query = `
+query {
+  transactions(
+    first: 100,
+    after: "${edges[edges.length - 1].cursor}", 
+    owners: ["${addr}"],
+    tags: {name: "Contract-Src", values: ["${TRADE_SOURCE_ID}"]}) {
+      edges {
+        cursor
+        node {
+          id
+        }
+      }
+    }
+}      
+      `
+      if (edges.length >= 100) {
+        return fetch(`${GATEWAY}/graphql`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            query
+          })
+        }).then(res => res.ok ? res.json() : Promise.reject(new Error('Could not get data')))
+          .then(path(['data', 'transactions', 'edges']))
+          .then(e => e.length + edges.length)
+      } else {
+        return edges.length
+      }
+    })
+
+}
+
+export const getStampCoinBalance = async (addr) => {
+  return fetch(`${CACHE}/${STAMP_CONTRACT}`).then(res => res.ok ? res.json() : Promise.reject(new Error('cache not found')))
+    .then(state => state.balances[addr] ? state.balances[addr] : 0)
+    .then(atomicToStamp)
+    .then(x => Number(x).toFixed(2))
+}
+
+export const getArBalance = async (addr) => {
+  return fetch(`${GATEWAY}/wallet/${addr}/balance`)
+    .then(res => res.ok ? res.json() : Promise.reject(new Error('could not get AR Balance')))
+    .then(winstonToAr)
+    .then(x => Number(x).toFixed(4))
+}
+
+export const getBARBalance = async (addr) => {
+  return fetch(`${CACHE}/${BAR}`).then(res => res.ok ? res.json() : Promise.reject(new Error('could not get bar balance')))
+    .then(state => state.balances[addr] ? state.balances[addr] : 0)
+    .then(atomicToBar)
+    .then(x => Number(x).toFixed(4))
+}
+
--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,58 +1,112 @@
 <script>
-  import { getAssetCount, getStampCount } from "../lib/app.js";
+  import {
+    getAssetCount,
+    getStampCount,
+    getUserAssets,
+    getStampCoinBalance,
+    getArBalance,
+    getBARBalance,
+  } from "../lib/app.js";
+  import Connect from "../dialogs/connect.svelte";
+  import Help from "../dialogs/wallet-help.svelte";
+  import { profile } from "../store.js";
+
+  let showConnect = false;
+  let showHelp = false;
 </script>
 
 <main>
   <section class="hero min-h-screen bg-base-100 items-start">
     <div class="hero-content flex-col">
-      <h1 class="text-6xl font-extrabold">STAMPS</h1>
+      <h1 class="text-6xl font-extrabold text-[#231F1F]">STAMPS</h1>
       <div class="stats">
         {#await getAssetCount() then count}
           <div class="stat place-items-center">
             <div class="stat-title">Tradeable Atomic Assets</div>
-            <div class="stat-value">{count}</div>
+            <div class="stat-value text-[#231F1F]">{count}</div>
           </div>
         {/await}
         {#await getStampCount() then count}
           <div class="stat place-items-center">
             <div class="stat-title">Total Stamps Created</div>
-            <div class="stat-value">{count}</div>
+            <div class="stat-value text-[#231F1F]">{count}</div>
           </div>
         {/await}
       </div>
-      <div class="border-2 border-gray-400 rounded-xl p-8">
-        <div class="flex flex-col items-center space-y-4 mb-8">
-          <h3 class="text-2xl font-bold">Rakis</h3>
-          <img
-            class="mask mask-circle h-[84px] w-[84px]"
-            src="https://arweave.net/fYmFNZbRCbPhBWqmOJLNiJFoLFiFchIBSZNI6jRwWaI"
-          />
-          <div class="text-sm">vh-NTHVvlKZqRxc8LyyTNok65yQ55a_PJ1zWLb9G2JI</div>
+      {#if $profile}
+        <div class="border-2 border-gray-400 rounded-xl p-8">
+          <div class="flex flex-col items-center space-y-4 mb-8">
+            <h3 class="text-2xl font-bold">
+              {$profile.profile.handleName !== ""
+                ? $profile.profile.handleName
+                : $profile.handle}
+            </h3>
+            <img
+              class="mask mask-circle h-[84px] w-[84px]"
+              src={$profile.profile.avatarURL}
+              alt={$profile.handle}
+            />
+            <div class="text-sm">
+              {$profile.addr}
+            </div>
+          </div>
+          <div class="stats shadow">
+            <div class="stat place-items-center">
+              <div class="stat-title">Atomic Assets</div>
+              <div class="stat-value">
+                {#await getUserAssets($profile.addr) then count}
+                  {count}
+                {/await}
+              </div>
+            </div>
+            <div class="stat place-items-center">
+              <div class="stat-title">StampCoin Balance</div>
+              <div class="stat-value">
+                {#await getStampCoinBalance($profile.addr) then count}
+                  {count}
+                {/await}
+              </div>
+            </div>
+            <div class="stat place-items-center">
+              <div class="stat-title">burned AR Balance</div>
+              <div class="stat-value">
+                {#await getBARBalance($profile.addr) then count}
+                  {count}
+                {/await}
+              </div>
+            </div>
+            <div class="stat place-items-center">
+              <div class="stat-title">AR Balance</div>
+              <div class="stat-value">
+                {#await getArBalance($profile.addr) then count}
+                  {count}
+                {/await}
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="stats shadow">
-          <div class="stat place-items-center">
-            <div class="stat-title">Atomic Assets</div>
-            <div class="stat-value">10</div>
-          </div>
-          <div class="stat place-items-center">
-            <div class="stat-title">StampCoin Balance</div>
-            <div class="stat-value">100</div>
-          </div>
-          <div class="stat place-items-center">
-            <div class="stat-title">burned AR Balance</div>
-            <div class="stat-value">1.20</div>
-          </div>
-          <div class="stat place-items-center">
-            <div class="stat-title">AR Balance</div>
-            <div class="stat-value">2.52</div>
-          </div>
+        <div>
+          <button
+            class="btn"
+            on:click={async () => {
+              await window.arweaveWallet.disconnect();
+              $profile = null;
+            }}>Disconnect</button
+          >
+          <!--
+          <button class="btn">Sell StampCoin</button>
+          <button class="btn">Purchase StampCoin</button>
+          -->
         </div>
-      </div>
-      <div>
-        <button disabled={true} class="btn">Get Your Stats</button>
-        <button class="btn">Sell StampCoin</button>
-        <button class="btn">Purchase StampCoin</button>
-      </div>
+      {:else}
+        <div class="h-[400px] grid place-items-center">
+          <button class="btn btn-primary" on:click={() => (showConnect = true)}
+            >Connect Wallet</button
+          >
+        </div>
+      {/if}
     </div>
   </section>
 </main>
+<Connect bind:open={showConnect} on:help={() => (showHelp = true)} />
+<Help bind:open={showHelp} />
